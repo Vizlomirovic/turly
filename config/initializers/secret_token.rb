9539e140f608bc29b11aa6b718dd608fefa719ca
Vedran Blazenka <vedran.blazenka@gmail.com>
2014-03-15 13:32:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turly::Application.config.secret_key_base = 'b377f8038d42366d57641094ff00d562a6975a8cc6aa73fffdbfb7159c8e6b2f79b01e0d31a9689c42048cbfcf2142d9e0b03fff800965a0c04de6861bc4db37'
