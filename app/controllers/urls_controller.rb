e368e149a414158259863988cdc3d99260483f67
Vedran Blazenka <vedran.blazenka@gmail.com>
2014-03-17 18:03:54 +0100

    added somesome error handling

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -9,21 +9,31 @@ class UrlsController < ApplicationController
 		s = (0...6).map { (65 + rand(26)).chr }.join.to_s.downcase
 
 		m_url = URI::parse(URI::encode(params[:url][:url]))
+
+		#check if parsed url is URL and then return error or success
 		unless m_url.kind_of?URI::HTTP or m_url.kind_of?URI::HTTPS
 			flash[:error] = "The URL you entered is not valid."
 		else
+			#generate random string and add it with url n
 			url = Url.create(:url=> Url.clean(params[:url]), :shorten=>s)
-			url.save
-			flash[:success] = "The url is created successfully. And shorten url is: "+"http://" + request.env["HTTP_HOST"] + "/" + s
+			if url.save
+				flash[:success] = "The url is created successfully. And shorten url is: "+"http://" + request.env["HTTP_HOST"] + "/" + s
+			else
+					flash[:error] = "The URL can't be created. Try again!"
+			end
 		end
-	
-	
 		
 		redirect_to index_path
 	end
 
 	def show
-		urls = Url.where(:shorten=>params[:link]).first
-		redirect_to urls.url
+		urls = Url.where(:shorten=>params[:link]).first rescue nil
+
+		if urls.nil? then 
+			flash[:error] = "The URL can't be found." 
+			redirect_to index_path 
+		else 
+			redirect_to urls.url 
+		end
 	end
 end
