1d556c7296781ee1ecf2ffdeba85519d36dfb810
Vedran Blazenka <vedran.blazenka@gmail.com>
2014-03-22 11:28:06 +0100

    added support for urls longer than 255 chars

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315154746) do
+ActiveRecord::Schema.define(version: 20140322102436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "urls", force: true do |t|
-    t.string   "url"
+    t.text     "url"
     t.text     "shorten"
     t.datetime "created_at"
     t.datetime "updated_at"
